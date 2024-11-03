15a0059a2fcaad460bf0e15851900597ba7e20e8
Narendran1424 <narendran1300@gmail.com>
2024-11-03 10:33:37 +0530

    Created Portfolio

---
+++ about.js
@@ -0,0 +1,84 @@
+const navLink1=document.querySelector(".nav-link1");
+const navIcon1=document.querySelector(".nav-icon1");
+const navLink2=document.querySelector(".nav-link2");
+const navIcon2=document.querySelector(".nav-icon2");
+const navLink3=document.querySelector(".nav-link3");
+const navIcon3=document.querySelector(".nav-icon3");
+const navLink4=document.querySelector(".nav-link4");
+const navIcon4=document.querySelector(".nav-icon4");
+const navLink5=document.querySelector(".nav-link5");
+const navIcon5=document.querySelector(".nav-icon5");
+navLink1.addEventListener("mouseover",()=>{
+    navLink1.style.color="orangeRed";
+    navIcon1.style.color="orangeRed";
+})
+navLink1.addEventListener("mouseout",()=>{
+    navLink1.style.color="black";
+    navIcon1.style.color="black";
+})
+
+navLink2.addEventListener("mouseout",()=>{
+    navLink2.style.color="black";
+    navIcon2.style.color="black";
+})
+navLink2.addEventListener("mouseover",()=>{
+    navLink2.style.color="orangeRed";
+    navIcon2.style.color="orangeRed";
+})
+navLink3.addEventListener("mouseout",()=>{
+    navLink3.style.color="black";
+    navIcon3.style.color="black";
+})
+navLink3.addEventListener("mouseover",()=>{
+    navLink3.style.color="orangeRed";
+    navIcon3.style.color="orangeRed";
+})
+navLink4.addEventListener("mouseover",()=>{
+    navLink4.style.color="orangeRed";
+    navIcon4.style.color="orangeRed";
+})
+navLink4.addEventListener("mouseout",()=>{
+    navLink4.style.color="black";
+    navIcon4.style.color="black";
+})
+navLink5.addEventListener("mouseover",()=>{
+    navLink5.style.color="orangeRed";
+    navIcon5.style.color="orangeRed";
+})
+navLink5.addEventListener("mouseout",()=>{
+    navLink5.style.color="black";
+    navIcon5.style.color="black";
+})
+
+const container=document.querySelector(".container");
+const hireBtn=document.getElementById("hire-me-btn");
+const closeBtn=document.getElementById("close");
+const hireBox=document.getElementById("hiring");
+const elementsToBlur = document.querySelectorAll("#heading, .detail-container, .qualification-container");
+
+
+hireBtn.addEventListener('click',()=>{
+    hireBox.style.display="flex";
+    hireBox.style.marginTop="100px"
+    elementsToBlur.forEach(element => {
+        element.classList.add("blur-effect"); // Add blur effect on each element
+    });
+
+})
+closeBtn.addEventListener('click',()=>{
+    hireBox.style.display="none";
+    elementsToBlur.forEach(element => {
+        element.classList.remove("blur-effect"); // Remove blur effect on each element
+    });
+})
+const elementsToRemoveBlur=document.querySelectorAll("#heading, .qualification-container,.skill-container,#detail-info,.navbar");
+    elementsToRemoveBlur.forEach(element=>{
+        element.addEventListener('click',()=>{
+            if(hireBox.style.display="flex"){
+                hireBox.style.display="none";
+            } 
+            elementsToBlur.forEach(element => {
+                element.classList.remove("blur-effect"); // Remove blur effect on each element
+            });
+        });
+    })
